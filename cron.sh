bba4f2a636467ef3e8a20512cfde30f2e7987439
realchandan <contact@chandan.cc>
2023-03-25 10:12:55 +0530

    created workflow

--- cron.sh
+++ cron.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
+rm -rf *.csv
 curl -L "https://cdn.jsdelivr.net/npm/@ip-location-db/geo-whois-asn-country/geo-whois-asn-country-ipv4-num.csv" -o "ipv4_db.csv"
 curl -L "https://cdn.jsdelivr.net/npm/@ip-location-db/geo-asn-country/geo-asn-country-ipv6-num.csv" -o "ipv6_db.csv"
